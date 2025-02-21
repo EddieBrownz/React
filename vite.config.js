5a3e56126afee82d22cf96a39749e3927a1df4e8
EddieBrownz <nourn57@gmail.com>
2025-02-21 15:28:27 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'React', // Change 'your-repo-name' to your GitHub repository name
+  base: '/React/', // Change 'your-repo-name' to your GitHub repository name
 
 })
